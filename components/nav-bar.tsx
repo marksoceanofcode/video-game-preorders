5ef3fbb5d765cc9975dd0926b2d0e2a6e509d03f
Mark Angell <markangell85@gmail.com>
2026-09-09 22:20:55 -0700

    WIP layout structure with navbar - Switched to pnpm

---
+++ components/nav-bar.tsx
@@ -0,0 +1,11 @@
+export const NavBar = () => {
+  return (
+    <div className="flex justify-center max-w-7xl mx-auto relative">
+      <nav className="bg-yellow-400 rounded-full absolute px-4 py-2 mt-2 sm:mt-4">
+        <ol className="inline">
+          <li>Home</li>
+        </ol>
+      </nav>
+    </div>
+  );
+};
